d55dacafb5af7d02ce2275c6731946d13c4dea1e
px <m@mamoco.se>
2024-06-05 16:52:07 +0200

    Added UNITORDERS array to store orders for the unit.

--- scripts/3d/unit/Unit.gd
+++ scripts/3d/unit/Unit.gd
@@ -14,6 +14,16 @@ var PLAYER : String = ""
 var UNITEQUIPMENT : Dictionary = {
 	"radio" : false,
 }
+var UNITORDERS : Array = [
+	## Acts as a stack in order to put unit-initiated actions on top.
+	## Once consumed the first order is popped off the top of the array.
+	## Consumation of the order happens when all entities have arrived in the 
+	## waypoints area of influence or something happens that erases the queue
+	## such as destruction of the unit, overwhelming fire, rout etc.
+	##
+	## {"patrol" : [start Vector3, end Vector3]},
+	## {"patrol" : "pos Vector3"}
+]
 
 func createUnitEntity(UnitID:String, UnitType:String, Company:String, Platoon:String, Squad:String, Player: String, UnitEquipment:Dictionary) -> UnitEntity:
 	# var new_unitEntity = unitEntityScene.instantiate()
