ab2534642188371d9700af4c36e192117d9dd780
px <m@mamoco.se>
2024-06-07 14:58:16 +0200

    Bättre hantering av multiselection

--- scripts/Selection.gd
+++ scripts/Selection.gd
@@ -12,8 +12,11 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-# func _process(delta: float) -> void:
-#	pass
+#var c = 0
+#func _process(delta: float) -> void:
+	#c += 1
+	#if int(c) % 50 == 0:
+		#prints("SelectedUnits: ",selectedUnits)
 
 func newUnitMarker(p := []):
 	print(p)
@@ -27,8 +30,13 @@ func getSelectedUnits() -> Array:
 	return []
 
 
+func inputHandled() -> void:
+	push_warning("Input should noe be handled")
+	get_node(".").get_viewport().set_input_as_handled()
+
 ## Handles selection of units. Stores them in the selectedUnits array.
 ## caller: MyUnitEntity
+## UnitEntity
 func select(caller, event) -> void:
 	if event is InputEventMouseButton and event.pressed == true:
 		"""
@@ -44,38 +52,68 @@ func select(caller, event) -> void:
 			var groups = caller.get_groups()
 			# if not SHIFT, then unselect other units and select this unit
 			if not Input.is_key_pressed(KEY_SHIFT):
-				for x in selectedUnits.size():
-					selectedUnits[x]._toggleSelected()
+				for x : UnitEntity in selectedUnits:
+					x._toggleSelected()
 				selectedUnits = []
 				var squad = caller.SQUAD
 				get_tree().call_group("squad-" + caller.SQUAD, "outline", 1)
 				caller._toggleSelected()
 				# add this unit to selectedUnits
 				selectedUnits = [caller]
-			else:
+				inputHandled()
+				
+			elif Input.is_key_pressed(KEY_SHIFT):
 				get_tree().call_group("squad-" + caller.SQUAD, "outline", 1)
 				caller._toggleSelected()
 				# shift is pressed, appending to selectedUnitssquad
 				if caller not in selectedUnits:
-					selectedUnits.append(caller)
+					selectedUnits.push_front(caller)
 					# prints("selectedUnits appended.", selectedUnits)
 				else: 
 					selectedUnits.erase(caller)
 					# prints("Removed caller:", str(caller), "from selectedunits.", selectedUnits)
+				inputHandled()
+				
 		elif event.button_index == 1 and Input.is_key_pressed(KEY_CTRL):
 			"""
 			Is CTRL pressed?
 			"""
-			get_tree().call_group("platoon-" + caller.PLATOON, "outline", 1)
-			var addUnits = get_tree().get_nodes_in_group("platoon-" + caller.PLATOON)
-			if not Input.is_key_pressed(KEY_SHIFT):
-				selectedUnits = []
+			print(caller.get_groups())
+			var addUnits : Array
+			if caller.UNITTYPE == "company-hq":
+				addUnits = get_tree().get_nodes_in_group("company-" + caller.COMPANY)
+			else:
+				addUnits = get_tree().get_nodes_in_group("platoon-" + caller.PLATOON)
+			var unitsToSelect : Array
 			for n in addUnits:
+				# sortera bort allt som inte är units
 				var unitClass = n.get_class()
-				if unitClass != "CharacterBody3D" and n not in selectedUnits:
-					selectedUnits.append(n)
-					n._toggleSelected()
-		print(selectedUnits)
+				if unitClass != "CharacterBody3D":
+					unitsToSelect.append(n)
+			
+			multiUnitSelection(unitsToSelect)
+
+
+func multiUnitSelection(units : Array) -> void:
+	## If KEY_SHIFT is pressed add the new units and dont empty selectedunits
+	## 
+	if Input.is_key_pressed(KEY_SHIFT):
+		for n : UnitEntity in units:
+			if n not in selectedUnits:
+				selectedUnits.append(n)
+				n.selectUnit()
+	else:
+		# empty selectedUnits
+		for n : UnitEntity in selectedUnits:
+			selectedUnits.erase(n)
+			n.unSelectUnit()
+		for n : UnitEntity in units:
+			if n not in selectedUnits:
+				selectedUnits.append(n)
+				n.selectUnit()
+		
+	inputHandled()
+
 
 func unselect(caller, event, position):
 	pass
@@ -90,7 +128,25 @@ func _unhandled_input(event):
 	if event is InputEventMouseButton \
 	and event.button_index == 1:
 	# TODO: Handle unselecting units by clicking anywhere else
+		print(event)
+		print(get_mouse_pos())
 		push_warning("Handle unselecting units by clicking anywhere else")
+
+func get_mouse_pos():
+	var space_state = get_parent().get_world_3d().get_direct_space_state()
+	var mouse_position = get_viewport().get_mouse_position()
+	var camera = get_tree().root.get_camera_3d()
+	
+	var ray_origin = camera.project_ray_origin(mouse_position)
+	var ray_end = ray_origin + camera.project_ray_normal(mouse_position) * 1000
+		
+	var params = PhysicsRayQueryParameters3D.new()
+	params.from = ray_origin
+	params.to = ray_end
+	#params.collision_mask = 2
+	params.exclude = []
+	
+	return space_state.intersect_ray(params)
 
 
 func showOrderLines() -> void:
