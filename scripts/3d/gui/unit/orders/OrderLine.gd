19e42cb165a02ee8c974c76710798f1547c90746
px <m@mamoco.se>
2024-06-07 10:56:00 +0200

    housekeeping

--- scripts/3d/gui/unit/orders/OrderLine.gd
+++ scripts/3d/gui/unit/orders/OrderLine.gd
@@ -7,7 +7,7 @@ func _init(from:Vector3, to:Vector3, color:Color) -> void:
 	var length = (from-to).length()
 	var radius := 0.2
 	var line := CylinderMesh.new()
-
+	
 	line.radial_segments = 8
 	line.bottom_radius = radius
 	line.top_radius = radius
@@ -21,15 +21,18 @@ func _init(from:Vector3, to:Vector3, color:Color) -> void:
 		line_array[ArrayMesh.ARRAY_VERTEX][i] = line_array[ArrayMesh.ARRAY_VERTEX][i].rotated(Vector3.RIGHT, PI/2.0)
 		line_array[ArrayMesh.ARRAY_NORMAL][i] = line_array[ArrayMesh.ARRAY_NORMAL][i].rotated(Vector3.RIGHT, PI/2.0)
 
-	var arr_mesh = ArrayMesh.new()
-	arr_mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, line_array)
-	
 	var mat = StandardMaterial3D.new()
 	mat.albedo_color = color
-	mat.transparency = 0.4
-	line.material = mat
+	mat.transparency = 0.5
+
+	var arr_mesh = ArrayMesh.new()
+	arr_mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, line_array)
+		
+	mesh = arr_mesh
 	
 	self.cast_shadow = SHADOW_CASTING_SETTING_OFF
-	self.transparency = 0.25
+	self.transparency = 0.5
+	self.material_override = mat
+	self.look_at_from_position(from, to, Vector3(0,1,0))
 	
-	mesh = arr_mesh
+	
